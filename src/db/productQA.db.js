e3ece373e79c622a98fe83f3dcd3db4759d33e14
VEDANTraj9 <manikantraj060@gmail.com>
2026-03-28 16:04:06 +0530

    backend QA Timing Problem and Price filter  Tour and Travels hardcoded

--- src/db/productQA.db.js
+++ src/db/productQA.db.js
@@ -2,18 +2,15 @@ import pool from "./productDB.js";
 
 /* ================= QUESTIONS ================= */
 
-
-
 export const createQuestion = async ({ product_id, user_id, question }) => {
   const [result] = await pool.execute(
     `INSERT INTO product_questions 
-     (product_id, user_id, question)
-     VALUES (?, ?, ?)`,
-    [product_id, user_id, question]
+     (product_id, user_id, question, created_at)
+     VALUES (?, ?, ?, DATE_ADD(NOW(), INTERVAL 330 MINUTE))`,
+    [product_id, user_id, question],
   );
   return result;
 };
-
 
 export const getQuestionsWithAnswers = async (product_id) => {
   const [rows] = await pool.execute(
@@ -28,21 +25,25 @@ export const getQuestionsWithAnswers = async (product_id) => {
      LEFT JOIN product_answers a ON a.question_id = q.id
      WHERE q.product_id = ?
      ORDER BY q.created_at DESC`,
-    [product_id]
+    [product_id],
   );
   return rows;
 };
 
 /* ================= ANSWERS ================= */
 
-
-
-export const createAnswer = async ({ question_id, product_id, user_id, user_type, answer }) => {
+export const createAnswer = async ({
+  question_id,
+  product_id,
+  user_id,
+  user_type,
+  answer,
+}) => {
   const [result] = await pool.execute(
     `INSERT INTO product_answers
-     (question_id, product_id, user_id, user_type, answer)
-     VALUES (?, ?, ?, ?, ?)`,
-    [question_id, product_id, user_id, user_type, answer]
+     (question_id, product_id, user_id, user_type, answer, created_at)
+     VALUES (?, ?, ?, ?, ?, DATE_ADD(NOW(), INTERVAL 330 MINUTE))`,
+    [question_id, product_id, user_id, user_type, answer],
   );
   return result;
 };
@@ -50,7 +51,7 @@ export const createAnswer = async ({ question_id, product_id, user_id, user_type
 export const getQuestionById = async (question_id) => {
   const [rows] = await pool.execute(
     "SELECT product_id FROM product_questions WHERE id = ?",
-    [question_id]
+    [question_id],
   );
   return rows[0];
 };
